bb23f9b3e54d4b3c083c4ec8de1a1e96067b140b
Midhun2k1 <midhunovm@gmail.com>
2026-04-10 16:43:57 +0530

    production database update.

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/messenger_db"
+#DATABASE_URL = "postgresql://postgres:admin@localhost:5432/messenger_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+ 
 
 engine = create_engine(DATABASE_URL)
 
