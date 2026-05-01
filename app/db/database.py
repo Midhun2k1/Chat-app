a51d277566bd6791944c4452394e1e0a13496c61
Midhun2k1 <midhunovm@gmail.com>
2026-05-01 17:15:23 +0530

    messenger password issue fix may 1st

--- app/db/database.py
+++ app/db/database.py
@@ -7,7 +7,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = os.getenv("DATABASE_URL")
  
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
